dff6b4af4fc3584a16f9a7b406ce57b8a5137c7c
akbar-Kalani <138216911+akbar-Kalani@users.noreply.github.com>
2023-08-11 23:39:09 +0330

    images

--- src/components/servicesOffered/servicesOffered.jsx
+++ src/components/servicesOffered/servicesOffered.jsx
@@ -9,11 +9,11 @@ import Image from 'next/image'
 const ServicesOffered = () => {
     return (
         <section className='flex flex-wrap-reverse justify-between items-center  mt-8'>
-            <figure className='mt-5 md:mt-0'>
+            <figure className='mt-5 md:mt-0 '>
                 <Image
-                    className='rounded-lg'
-                    src="/img/Anetteskog.png"
-                    width={50}
+                    className='rounded-lg '
+                    src="/img/img1.jpg"
+                    width={500}
                     height={200}
                     alt='image'
                 />
